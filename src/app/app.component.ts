666ccd2b8da7ad0dc3c35441f932f239655c4cd9
Marlon Santos <marlonergondossantos@gmail.com>
2020-04-04 20:05:26 -0300

    study angular - *ngIf and ngTemplate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'pokedex';
+  test = true;
+  title = ["pokedex1", "pokedex2", "pokdex3"];
+  back = { dados: { nome: { user: "marlon" } } };
 }
---
+++ src/app/photos/photos.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PhotoComponent } from './photo/photo.component';
+
+
+
+@NgModule({
+  declarations: [PhotoComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PhotosModule { }
