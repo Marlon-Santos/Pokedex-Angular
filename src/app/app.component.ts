18ce654c65227bf84840788879a57e0c2dff66c9
Marlon Santos <marlonergondossantos@gmail.com>
2020-04-05 20:19:35 -0300

    use event and property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,17 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
+  pkm = null;
   test = true;
   mod = "testNhgModel";
   title = ["pokedex1", "pokedex2", "pokdex3"];
   back = { dados: { nome: { user: "marlon" } } };
+  get pkmFn() {
+    const link = ("000" + this.pkm.number).slice(-3);
+    console.log(`//serebii.net/sunmoon/pokemon/${link}.png`);
+    return `//serebii.net/sunmoon/pokemon/${link}.png`;
+  }
+
   pokedex = [
     { name: "pokemon", number: 1 },
     { name: "pokemon", number: 2 },
