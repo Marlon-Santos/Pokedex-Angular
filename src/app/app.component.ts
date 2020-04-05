1326d07e76f676d0febd2298586de8fa186c10cd
Marlon Santos <marlonergondossantos@gmail.com>
2020-04-04 21:22:49 -0300

    use *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,12 @@ export class AppComponent {
   test = true;
   title = ["pokedex1", "pokedex2", "pokdex3"];
   back = { dados: { nome: { user: "marlon" } } };
+  pokedex = [
+    { name: "pokemon", number: 1 },
+    { name: "pokemon", number: 2 },
+    { name: "pokemon", number: 4 },
+    { name: "pokemon", number: 5 },
+    { name: "pokemon", number: 6 },
+    { name: "pokemon", number: 7 },
+  ];
 }
