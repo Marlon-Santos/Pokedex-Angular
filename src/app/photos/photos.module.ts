1b5436050d891d1742117fa93ada945c852f2800
Marlon Santos <marlonergondossantos@gmail.com>
2020-04-15 00:53:10 -0300

    use Pipe

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { PhotoComponent } from "./photo/photo.component";
 import { ColorRandomDirective } from "./directives/color-random.directive";
-import { NgUnlessDirective } from './directives/ng-unless.directive';
+import { NgUnlessDirective } from "./directives/ng-unless.directive";
+import { ReversePipe } from "./pipes/reverse.pipe";
 
 @NgModule({
-  declarations: [PhotoComponent, ColorRandomDirective, NgUnlessDirective],
+  declarations: [
+    PhotoComponent,
+    ColorRandomDirective,
+    NgUnlessDirective,
+    ReversePipe,
+  ],
   imports: [CommonModule],
-  exports: [ColorRandomDirective, PhotoComponent],
+  exports: [ColorRandomDirective, PhotoComponent, ReversePipe],
 })
 export class PhotosModule {}
