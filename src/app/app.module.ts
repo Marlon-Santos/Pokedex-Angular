45944bc2189527a1a4a37586aacadb2d4f299a79
Marlon Santos <marlonergondossantos@gmail.com>
2020-04-15 23:13:43 -0300

    use ngForm and ngModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { PhotosModule } from "./photos/photos.module";
-import { PhotoComponent } from "./photos/photo/photo.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { HttpClientModule } from "@angular/common/http";
+import { FormComponent } from "./form/form.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, FormComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule, PhotosModule],
   providers: [],
   bootstrap: [AppComponent],
